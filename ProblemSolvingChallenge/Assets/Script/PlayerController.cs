a9937c28424f79ac31bfc54705ecfef29697fde4
MWIMAF <mwimaf@student.telkomuniversity.ac.id>
2021-10-12 22:23:05 +0700

    Problem2

---
+++ ProblemSolvingChallenge/Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+
+
+    private Rigidbody2D rig;
+    private Vector2 velocity;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+        velocity = new Vector2(moveAccel, 0);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        rig.MovePosition(rig.position + velocity * Time.fixedDeltaTime);
+
+    }
+}
